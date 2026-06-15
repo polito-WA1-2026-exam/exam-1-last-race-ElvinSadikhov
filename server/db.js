bfb077f176341a94c9c388b89074156ed0f6279d
elvinsadikhov <elvin.279.6s@gmail.com>
2026-06-15 19:40:31 +0200

    chore: add database connection module

---
+++ server/db.js
@@ -0,0 +1,12 @@
+import sqlite3 from 'sqlite3';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const db = new sqlite3.Database(
+  path.join(__dirname, 'database.db'),
+  err => { if (err) throw err; }
+);
+
+export default db;
